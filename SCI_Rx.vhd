fc4a47bf351b3675558935ab812103d7520737d6
Maya Purohit <131403325+MayaPurohit@users.noreply.github.com>
2025-05-28 16:53:01 -0400

    Update SCI_Rx.vhd

--- SCI_Rx.vhd
+++ SCI_Rx.vhd
@@ -6,7 +6,6 @@ entity SCI_Rx is
   port (
     clk          : in  std_logic;
     Rx           : in  std_logic;
-    Receive_en   : in  std_logic;
     Parallel_out : out std_logic_vector(7 downto 0);
     Rx_done      : out std_logic;
     Valid        : out std_logic
@@ -24,7 +23,7 @@ architecture Behavioral of SCI_Rx is
   type state_type is (idle, wait_half, shift, wait_full, data_ready);
   signal CS, NS : state_type := idle;
 
-  signal shift_reg       : std_logic_vector(9 downto 0) := (others => '0');
+  signal shift_reg       : std_logic_vector(7 downto 0) := (others => '0');
   signal baud_counter    : unsigned(9 downto 0) := (others => '0');
   signal bit_counter     : unsigned(3 downto 0) := (others => '0');
 
@@ -57,12 +56,12 @@ begin
   ---------------------------------------
   -- Next State Logic
   ---------------------------------------
-  NextStateLogic: process(CS, Rx, tc_baud, tc_half_baud, tc_bit, Receive_en)
+  NextStateLogic: process(CS, Rx, tc_baud, tc_half_baud, tc_bit)
   begin
     NS <= CS;
     case CS is
       when idle =>
-        if Receive_en = '1' and Rx = '0' then
+        if Rx = '0' then
           NS <= wait_half;
         end if;
 
@@ -126,9 +125,9 @@ begin
         rx_done_int <= '1';
         clr_bit     <= '1';
         -- check for valid ASCII
-        if to_integer(unsigned(shift_reg(8 downto 1))) < 32 or
-           to_integer(unsigned(shift_reg(8 downto 1))) > 122 or (to_integer(unsigned(shift_reg(8 downto 1))) > 32 and to_integer(unsigned(shift_reg(8 downto 1)))<48) or 
-           (to_integer(unsigned(shift_reg(8 downto 1))) > 57 and to_integer(unsigned(shift_reg(8 downto 1)))<97) then
+        if to_integer(unsigned(shift_reg)) < 32 or
+           to_integer(unsigned(shift_reg)) > 122 or (to_integer(unsigned(shift_reg)) > 32 and to_integer(unsigned(shift_reg))<48) or 
+           (to_integer(unsigned(shift_reg)) > 57 and to_integer(unsigned(shift_reg))<97) then
           valid_int <= '0';
         end if;
 
@@ -158,7 +157,7 @@ begin
     end if;
     
     if rising_edge(clk) then 
-     if tc_baud = '1' or tc_half_baud = '1' then
+     if (tc_baud = '1' or tc_half_baud = '1') then
         bit_counter <= bit_counter + 1;
       end if;
      if clr_bit = '1' then
@@ -175,7 +174,7 @@ begin
   begin
     if rising_edge(clk) then
       if shift_en = '1' then
-        shift_reg <= Rx & shift_reg(9 downto 1);
+        shift_reg <= Rx & shift_reg(7 downto 1);
       end if;
       if clr_shift_reg = '1' then
         shift_reg <= (others => '0');
@@ -206,7 +205,7 @@ begin
   bit_check: process(bit_counter)
   begin
     tc_bit <= '0';
-    if bit_counter = BIT_COUNT - 1 then
+    if bit_counter = BIT_COUNT then
       tc_bit <= '1';
     end if;
   end process bit_check;
@@ -214,8 +213,9 @@ begin
   ---------------------------------------
   -- Output assignments
   ---------------------------------------
-  Parallel_out <= shift_reg(8 downto 1);  -- exclude start/stop bits
+  Parallel_out <= shift_reg;  -- exclude start/stop bits
   Rx_done      <= rx_done_int;
   Valid        <= valid_int;
-
 end Behavioral;
+        
+        
